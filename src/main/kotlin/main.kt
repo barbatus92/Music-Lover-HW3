6248bc5e85c264c090bd4c5ef4d9c9eccc22cf77
Alexander Kashenko <helasot@gmail.com>
2022-10-12 15:38:38 +0600

    corrected remarks

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,21 @@
 fun main() {
-    val amount = 1200
-    val discount1 = amount * 0  //Если сумма покупки от 0 до 1 000 рублей, то никаких скидок нет.
-    val discount2 = 100   //Если сумма покупки от 1 001 до 10 000 рублей, то стандартная скидка - 100 рублей
-    val discount3 = amount * 0.05 //Если сумма покупки от 10 001 рубль и выше то % составляет 5% от суммы.
-    val discount4 = 0.01 //Если покупатель- постоянный клиент
+    val amount = 150
+    val discountZero = amount * 0  //Если сумма покупки от 0 до 1 000 рублей, то никаких скидок нет.
+    val discountMid =
+        100   //Если сумма покупки от 1 001 до 10 000 рублей, то стандартная скидка - 100 рублей
+    val discountMax = (amount * 0.05) //Если сумма покупки от 10 001 рубль и выше то % составляет 5% от суммы.
+    val discountRegularBuyer = 0.01 //Если покупатель- постоянный клиент
     val statusOfBuyer = true // true- постоянный покупатель, false- непостоянный.
+    var totalDiscount = 0
 
-
-    val totalDiscount = when (amount) {
-        in 0..1000 -> if (statusOfBuyer) discount4 else discount1
-        in 1001..10000 -> if (statusOfBuyer) discount2 + (amount - discount2) * discount4 else discount2
-        in 10001..Int.MAX_VALUE -> if (statusOfBuyer) discount3 + (amount - discount3) * discount4 else discount3
+    when (amount) {
+        in 0..1000 -> totalDiscount = discountZero
+        in 1001..10000 -> totalDiscount = discountMid
+        in 10001..Int.MAX_VALUE -> totalDiscount = discountMax.toInt()
         else -> "not rated"
     }
-
-    println("Скидка составит: $totalDiscount RUB")
-
+    if (statusOfBuyer) { // проверка на статус постоянного покупателя
+        totalDiscount = ((amount - totalDiscount) * discountRegularBuyer + totalDiscount).toInt()
+    }
+    print(" Сумма покупки: $amount рублей \n Ваша скидка составит : $totalDiscount рублей")
 }
